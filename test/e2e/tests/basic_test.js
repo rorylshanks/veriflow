2ce8abf57f659696dbfe0f2ec794daf76326b733
Rory Shanks <rorylshanks@gmail.com>
2023-11-08 17:26:25 +0100

    Fixed tests

--- test/e2e/tests/basic_test.js
+++ test/e2e/tests/basic_test.js
@@ -49,5 +49,5 @@ Scenario('Testing Token Auth', async ({ I }) => {
 Scenario('Testing Unauthorized Flow', async ({ I }) => {
     I.amOnPage('http://test-unauthorized-login.localtest.me:2080/');
     I.login()
-    I.see("Unauthorized")
+    I.see("User is not authorized to access this route.")
 });
