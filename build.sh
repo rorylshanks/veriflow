f25fed01f15dd986ddc85fcf7045d24d653ea8eb
Rory Shanks <rorylshanks@gmail.com>
2024-03-03 15:37:41 +0100

    Added tokenclaims idp_adapter

--- build.sh
+++ build.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-docker buildx create --use --name multi --platform linux/arm64,linux/amd64
 docker buildx build \
   --platform linux/arm64,linux/amd64 \
-  -t rorylshanks/veriflow:latest \
+  -t rorylshanks/veriflow:debug \
   --push \
   -f Dockerfile \
   .
-docker buildx rm multi
 
