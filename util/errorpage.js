75cf9cf5b40ae97cd34ebef01cde28b0925f1ffa
Rory Shanks <6383578+rorylshanks@users.noreply.github.com>
2024-03-16 23:30:12 +0100

    Added new UI for error pages, and added ability to log user out of all sessions (#21)

--- util/errorpage.js
+++ util/errorpage.js
@@ -1,13 +1,32 @@
 import ejs from 'ejs';
 import path from 'path';
+import { getConfig, getRedirectBasepath } from '../util/config.js';
 
-async function renderErrorPage(status_code, error_code_override) {
+
+async function renderErrorPage(status_code, error_code_override, req) {
+    var config = getConfig();
+    var redirectBasePath = getRedirectBasepath()
+    var logoutUrlObj = new URL(`${config.service_url}${redirectBasePath}/logout`)
+    var logout_url = logoutUrlObj.href
     var header = "Internal Server Error"
+
+
+    // UI Tweaks for error page
+    var footer_text = config?.ui?.error_page_footer_text || "Veriflow Access Proxy"
+    var background_image_url = config?.ui?.error_page_background || false
+    var additional_css = config?.ui?.error_page_additional_css || false
+    var show_error_code = true
+    if (config?.ui?.error_page_show_error_code != null){
+        var show_error_code = config?.ui?.error_page_show_error_code
+    }
+    var logo_image_src = config?.ui?.logo_image_src || false
+    var page_title = config?.ui?.page_title || "Veriflow"
+
     var description = "An internal server error occoured. Please try again."
     var error_code = "ERR_INTERNAL_ERROR"
     if (status_code == 404) {
-        header = "Resource Not Found"
-        description = "The requested resource cannot be found. Please check and try again."
+        header = "Page Not Found"
+        description = "The requested page cannot be found. Please check and try again."
         error_code = "ERR_NOT_FOUND"
     }
     if (status_code == 403) {
@@ -23,16 +42,31 @@ async function renderErrorPage(status_code, error_code_override) {
     if (error_code_override) {
         error_code = error_code_override
     }
-    var title = `${status_code} ${header}`
-    var html = await ejs.renderFile(path.join(process.cwd(), '/views/error_fullpage.ejs'), {
+
+    if (error_code == "LOGOUT_SUCCESS") {
+        header = "Logged Out"
+        description = "You have been successfully logged out of Veriflow."
+    }
+
+    var title = `${status_code} ${header} | ${page_title}`
+    var html = await ejs.renderFile(path.join(process.cwd(), '/views/error_fullpage.ejs'), 
+    {
         title,
         header,
         status_code,
         description,
-        error_code
+        error_code,
+        footer_text,
+        user: req?.session?.userId,
+        logout_url: logout_url,
+        background_image_url: background_image_url,
+        additional_css: additional_css,
+        show_error_code: show_error_code,
+        logo_image_src: logo_image_src
     });
     return html
 }
+
 
 export default {
     renderErrorPage
