143f9541f7d58a39489ec377e6379347e2c644df
Rory Shanks <6383578+rorylshanks@users.noreply.github.com>
2024-03-17 01:02:31 +0100

    Added maxAge to cookie to be more secure by default, auth service now… (#23)
    * Added maxAge to cookie to be more secure by default, auth service now only listens on localhost for security reasons
    
    * Removed old config var no longer used
    
    * Added default maxAge to cookie for security reasons
    
    * Fixed a bug with setting of cookie expiry

--- test/e2e/tests/basic_test.js
+++ test/e2e/tests/basic_test.js
@@ -1,39 +1,39 @@
 Feature('Login').retry(3);
 
 Scenario('Basic login test', async ({ I }) => {
-    I.amOnPage('http://test-basic-login.localtest.me:2080/get');
+    I.amOnPage('http://test-basic-login.localtest.me/get');
     I.login();
     I.see("x-veriflow-user-id")
 });
 
 Scenario('HTTPS Upstream Test', async ({ I }) => {
-    I.amOnPage('http://test-https-upstream.localtest.me:2080/get');
+    I.amOnPage('http://test-https-upstream.localtest.me/get');
     I.login();
     I.see("x-veriflow-user-id")
 });
 
 Scenario('Removing headers', async ({ I }) => {
-    I.amOnPage('http://test-removing-headers.localtest.me:2080/get');
+    I.amOnPage('http://test-removing-headers.localtest.me/get');
     I.login();
     I.dontSee("x-veriflow-user-id")
 });
 
 Scenario('Adding headers', async ({ I }) => {
-    I.amOnPage('http://test-adding-headers.localtest.me:2080/get');
+    I.amOnPage('http://test-adding-headers.localtest.me/get');
     I.login();
     I.see("x-pomerium-claim-email")
     I.see("x-test-header")
 });
 
 Scenario('Testing mTLS', async ({ I }) => {
-    I.amOnPage('http://test-mtls-auth.localtest.me:2080/');
+    I.amOnPage('http://test-mtls-auth.localtest.me/');
     I.login();
     I.see("Veriflow-Test-Cert")
     I.see("Veriflow-Test-CA")
 });
 
 Scenario('Testing Header Mapping', async ({ I }) => {
-    I.amOnPage('http://test-header-mapping.localtest.me:2080/');
+    I.amOnPage('http://test-header-mapping.localtest.me/');
     I.login();
     I.see("ThisIsATestHeaderFromTheHeaderMapping")
     I.see("TestHeaderFromGroup")
@@ -41,7 +41,7 @@ Scenario('Testing Header Mapping', async ({ I }) => {
 });
 
 Scenario('Testing Header Mapping Inline', async ({ I }) => {
-    I.amOnPage('http://test-header-mapping-inline.localtest.me:2080/');
+    I.amOnPage('http://test-header-mapping-inline.localtest.me/');
     I.login();
     I.see("ThisIsATestHeaderFromTheHeaderMapping")
     I.see("TestHeaderFromGroup")
@@ -52,37 +52,37 @@ Scenario('Testing Token Auth', async ({ I }) => {
     I.setPuppeteerRequestHeaders({
         'Authorization': 'Bearer ThisIsATestToken',
     });
-    I.amOnPage('http://test-token-auth.localtest.me:2080/');
+    I.amOnPage('http://test-token-auth.localtest.me/');
     I.see("x-veriflow-user-id")
 });
 
 Scenario('Testing Unauthorized Flow', async ({ I }) => {
-    I.amOnPage('http://test-unauthorized-login.localtest.me:2080/');
+    I.amOnPage('http://test-unauthorized-login.localtest.me/');
     I.login()
     I.see("ERR_NOT_AUTHORIZED")
 });
 
 Scenario('Advanced matchers test', async ({ I }) => {
-    I.amOnPage('http://test-advanced-matchers.localtest.me:2080/get');
+    I.amOnPage('http://test-advanced-matchers.localtest.me/get');
     I.login();
     I.see("x-veriflow-user-id")
-    I.amOnPage('http://test-advanced-matchers.localtest.me:2080/should404');
+    I.amOnPage('http://test-advanced-matchers.localtest.me/should404');
     I.see("ERR_ROUTE_NOT_FOUND")
 });
 
 Scenario('Dynamic Upstreams Test', async ({ I }) => {
-    I.amOnPage('http://test-dynamic-upstreams.localtest.me:2080/get');
+    I.amOnPage('http://test-dynamic-upstreams.localtest.me/get');
     I.login();
     I.see("x-veriflow-user-id")
 });
 
 Scenario('Dynamic Upstreams Test SRV', async ({ I }) => {
-    I.amOnPage('http://test-dynamic-upstreams-srv.localtest.me:2080/get');
+    I.amOnPage('http://test-dynamic-upstreams-srv.localtest.me/get');
     I.login();
     I.see("x-veriflow-user-id")
 });
 
 Scenario('Unauthenticated Access test', async ({ I }) => {
-    I.amOnPage('http://test-unauthenticated-access.localtest.me:2080/get');
+    I.amOnPage('http://test-unauthenticated-access.localtest.me/get');
     I.see("x-public-access")
 });
